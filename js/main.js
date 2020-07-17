31b0099371eacf8952687425b6314bf3d45dc262
Duru-young Raymond <duruyoungcr@gmail.com>
2020-07-17 11:04:25 +0100

    updated main.js

--- js/main.js
+++ js/main.js
@@ -42,68 +42,61 @@ modal.addEventListener("click", (e) => {
 });
 
 //email validation
-// const topForm = document.querySelector(".top-form");
-// const bottomForm = document.querySelector(".bottom-form");
-// const topInput = document.querySelector(".top-form input");
-// const bottomInput = document.querySelector(".bottom-form input");
+const topForm = document.querySelector(".top-form");
+const bottomForm = document.querySelector(".bottom-form");
+const topInput = document.querySelector(".top-form input");
+const bottomInput = document.querySelector(".bottom-form input");
 
 $(function () {
-  $("form").submit(function (e) {
+  $(".top-form").submit(function (e) {
     //prevent default form submission
     e.preventDefault();
-
     //ajax form submission
-    $.ajax({
-      type: "POST",
-      url: "form.php",
-      data: $(this).serialize(),
-      success: (message) => {
+    if (topInput.value !== "") {
+      $.ajax({
+        type: "POST",
+        url: "form.php",
+        data: $(this).serialize(),
+        success: (message) => {
+          // add an action to be carried out when submission is successful
+          topForm.reset();
+          swal("Success", "Subscription successful", "success");
+        },
         // add an action to be carried out when submission is successful
-        swal("Success", "Subscription successful", "success");
-      },
-      // add an action to be carried out when submission is successful
-      error: (error) => {
-        swal("Error", "Please enter a valid email", "error");
-      },
-    });
+        error: (error) => {
+          swal("Error", "Please enter a valid email", "error");
+        },
+      });
+    } else {
+      swal("Error", "Email cannot be empty!", "error");
+    }
   });
 });
-
-// topForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   if (topInput.value == "") {
-//     swal("Error", "Email cannot be empty!", "error");
-//   } else {
-//     checkInputs();
-//   }
-// });
-
-// bottomForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   if (bottomInput.value == "") {
-//     swal("Error", "Email cannot be empty!", "error");
-//   } else {
-//     checkInputs();
-//   }
-// });
-
-// function checkInputs() {
-//   const email2 = bottomInput.value.trim();
-//   const email = topInput.value.trim();
-//   if (isEmail(email)) {
-//     swal("Success", "Subscription successful", "success");
-//     topForm.reset();
-//   } else {
-//     swal("Error", "Please enter a valid email", "error");
-//   }
-
-//   if (isEmail(email2)) {
-//     swal("Success", "Subscription successful", "success");
-//     bottomForm.reset();
-//   } else {
-//     swal("Error", "Please enter a valid email", "error");
-//   }
-// }
+$(function () {
+  $(".top-form").submit(function (e) {
+    //prevent default form submission
+    e.preventDefault();
+    //ajax form submission
+    if (bottomInput.value !== "") {
+      $.ajax({
+        type: "POST",
+        url: "form.php",
+        data: $(this).serialize(),
+        success: (message) => {
+          // add an action to be carried out when submission is successful
+          bottomForm.reset();
+          swal("Success", "Subscription successful", "success");
+        },
+        // add an action to be carried out when submission is successful
+        error: (error) => {
+          swal("Error", "Please enter a valid email", "error");
+        },
+      });
+    } else {
+      swal("Error", "Email cannot be empty!", "error");
+    }
+  });
+});
 
 // function isEmail(email) {
 //   const regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
